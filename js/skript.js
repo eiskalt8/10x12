dc131c9d806033e72019bdd786ec2e74018cf35e
eiskalt8 <einspamspam@gmail.com>
2023-11-01 16:47:53 +0100

    buttons

--- js/skript.js
+++ js/skript.js
@@ -1,3 +1,35 @@
+/*function safe_input() {
+    const input = document.getElementById(inputid).value;
+    localStorage.setItem(inputid, input);
+}
+
+function get_input(inputid) {
+    return localStorage.getItem(inputid)
+}
+*/
+//handle name
+function safe_name() {
+    const name = document.getElementById("username-input").value
+    localStorage.setItem("username", name)
+
+}
+
+function get_name() {
+    return localStorage.getItem("username")
+}
+
+//create room
+function create_room() {
+
+}
+
+//join existing room
+//create room
+function join_room() {
+
+}
+
+//Darkmode
 if (localStorage.getItem("darkMode") === true) {
             document.documentElement.setAttribute('data-bs-theme', 'dark')
     }
