f0cf15c66442061290e61055a6a063d1f64cb0e8
eiskalt8 <einspamspam@gmail.com>
2023-12-19 16:22:51 +0100

    make images and js accessible

--- backend/10x12.py
+++ backend/10x12.py
@@ -28,10 +28,20 @@ def check_room(room):
         return False  # User should not be able to join
 
 
+#TODO check if possible to combine these (until 44) in one static folder or something
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory("../website", "logo.ico")
 
+
+@app.route('/Logo_name.png')
+def logo():
+    return send_from_directory("../website", "Logo_name.png")
+
+
+@app.route('/js/skript.js')
+def js():
+    return send_from_directory("../website/js", 'skript.js')
 
 if __name__ == '__main__':
     app.run(debug=True)
