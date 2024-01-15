9e82fcb125c8b6ad8093717e22af3e58ca557d8a
eiskalt8 <einspamspam@gmail.com>
2024-01-15 15:59:41 +0100

    change button darkmode and add current_player in init

--- backend/sqlite_init.py
+++ backend/sqlite_init.py
@@ -7,7 +7,7 @@ cursor = conn.cursor()
 # create Tables Users and Sessions
 cursor.execute("CREATE TABLE IF NOT EXISTS Users (UserID CHAR(36) PRIMARY KEY, UserName VARCHAR(20), last_used DATE)")
 cursor.execute("CREATE TABLE IF NOT EXISTS Sessions (SessionID INTEGER PRIMARY KEY, Users TEXT, last_used DATE, "
-               "locked BOOLEAN DEFAULT 0)")
+               "locked BOOLEAN DEFAULT 0, current_player CHAR(36))")
 
 # execute and close
 conn.commit()
